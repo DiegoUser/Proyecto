576ea5c1a46d4aa52cd5cb2131df9906b62d5e6d
DiegoUser <garcia_mendez_diego@hotmail.com>
2021-09-28 10:24:48 -0300

    commit

--- index.php
+++ index.php
@@ -1,21 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bona+Nova&family=PT+Sans+Narrow&family=Quintessential&display=swap" rel="stylesheet">
+    <script type="text/javascript" src="script.js"></script>
     <title>Electro.Web</title>
 </head>
 <body>
     <div class="contenedor">
         <div class="banner">
-
+            <h1>hola<h1>
         </div>
         <div class="botones">
-
+        <h2>hola2<h2>
         </div>
         <div class="cuerpo">
-
+        <h3>hola3</h3>
         </div>
 
 
