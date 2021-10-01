4e8100bced489136d02c548f534ddb6776ccd35d
DiegoUser <garcia_mendez_diego@hotmail.com>
2021-10-01 03:44:59 -0300

    commit

--- ProcesoMOD.php
+++ ProcesoMOD.php
@@ -1,20 +1,20 @@
 <?php
     include "conexion.inc";
-    // capturar datos del formulario
     $id          = $_POST["ID"];
     $descripcion = utf8_decode($_POST["DES"]);
     $origen      = $_POST["ORG"];
     $precio      = $_POST["PRC"];
     // crear sentencia SQL para actualizar
     $sql  = "UPDATE electro SET ";
-    $sql .= "desELEC='$descripcion', ";
-    $sql .= "orgELEC='$origen', ";
-    $sql .= "prcELEC='$precio', ";
-    $sql .= "WHERE idELEC=$id";
+    $sql .= "desELEC='$descripcion',";
+    $sql .= "orgELEC='$origen',";
+    $sql .= "prcELEC='$precio',";
+    $sql .= "WHERE idELEC=$id";                
     // ejecutar sentencia SQL
     mysqli_query($conex,$sql);
     // cerrar conexión
     mysqli_close($conex);
-    // volver al formulario automáticamente
+    // volver al forumulario automáticamente
     header("Location: FormMOD.php");
 ?>
+    
