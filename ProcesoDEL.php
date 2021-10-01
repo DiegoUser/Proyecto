79bd8d8ddea3963f6f2f6bd14c25ea78ce7465b7
DiegoUser <garcia_mendez_diego@hotmail.com>
2021-10-01 02:03:28 -0300

    commit

--- ProcesoDEL.php
+++ ProcesoDEL.php
@@ -1,18 +1,13 @@
 <?php
-    // PROCESO PERSONAS DEL
-    
-    // conectar al servidor de Base de Datos
     include "conexion.inc";
     // capturar datos del formulario
     $id = $_POST["ID"];
     // crear sentencia SQL para eliminar registro físicamente
-    $sql  = "DELETE FROM personas WHERE idPERS=$id ";
-    // depurar SQL
-    // die($sql);
+    $sql  = "DELETE FROM electro WHERE idELEC=$id ";
     // ejecutar sentencia SQL
     mysqli_query($conex,$sql);
     // cerrar conexión
     mysqli_close($conex);
     // volver al forumulario automáticamente
-    header("Location: FormPersonasDEL.php");
+    header("Location: FormDEL.php");
 ?>
