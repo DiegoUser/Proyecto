e44f1d78f76aa04a3f5df449ebe2694118bba15c
DiegoUser <garcia_mendez_diego@hotmail.com>
2021-10-01 01:00:37 -0300

    commit

--- ProcesoINS.php
+++ ProcesoINS.php
@@ -1,8 +1,22 @@
 <?php
+// CONEXIÓN AL SERVIDOR DE BASE DE DATOS
+$conex = mysqli_connect("localhost","root","");
+// controlar conexión
+if (!$conex) {
+    // enviar mensaje de error
+    header("Location: errorPage.php?MSG=NO se pudo CONECTAR al SERVIDOR de Base de Datos");
+} // endif
+// seleccionar Base de Datos
+$selDB = mysqli_select_db($conex,"ELECTRO");
+// controlar selección de Base de Datos
+if (!$selDB) {
+    // enviar mensaje de error
+    header("Location: errorPage.php?MSG=NO se pudo SELECCIONAR Base de Datos");
+} // endif
     // PROCESO PERSONAS INS
     
     // conectar al servidor de Base de Datos
-    include "conexion.inc";
+    /*include "conexion.inc";*/
     // capturar datos del formulario
     $descripcion = utf8_decode($_POST["DES"]);
     $origen      = utf8_decode($_POST["ORG"]);
