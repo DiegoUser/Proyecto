37f505b0d0d8e99be7bc9c76c8cafe2d0a859930
DiegoUser <garcia_mendez_diego@hotmail.com>
2021-10-03 11:39:54 -0300

    commit

--- ProcesoFiltro.php
+++ ProcesoFiltro.php
@@ -2,7 +2,7 @@
     include "conexion.inc";
     $origen      = strtoupper($_POST["ORG"]);
     // crear sentencia SQL para actualizar
-    $sql = "SELECT * FROM electro WHERE origen = $origen";           
+    $sql = "SELECT * FROM electro WHERE orgELEC = $origen";           
     // ejecutar sentencia SQL
     mysqli_query($conex,$sql);
     // cerrar conexión
