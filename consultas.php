3b3201b32fa5d510aaa7dcd8b400ee50b38836aa
DiegoUser <garcia_mendez_diego@hotmail.com>
2021-10-02 23:41:39 -0300

    commit

---
+++ consultas.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Birthstone&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bona+Nova&family=PT+Sans+Narrow&family=Quintessential&display=swap" rel="stylesheet">
+    <script type="text/javascript" src="script.js"></script>
+    <title>Electro.Web</title>
+</head>
+
+<body>
+    <div class="contenedor">
+        <?php include "botones.inc" ?>
+
+    </div>
+</body>
+
+</html>
