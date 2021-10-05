320e9b624eb5e4082330f8db893adbca2cc582df
DiegoUser <garcia_mendez_diego@hotmail.com>
2021-10-05 10:18:45 -0300

    commit

--- consultas.php
+++ consultas.php
@@ -25,7 +25,7 @@
                             <h4>Origen:</h4>
                         </td>
                         <td>
-                            <input id="dataORG" type="text" name="ORG" maxlenght="10" title="Máximo 10 carácteres" />
+                            <input id="dataORG" style="text-transform:uppercase;" onkeyup="javascript:this.value=this.value.toUpperCase();" type="text" name="ORG" maxlenght="10" title="Máximo 10 carácteres" />
                         </td>
                     </tr>
                     <tr>
