3827d58d60d0c4093dae1d7606ac0eb61e63d23f
DiegoUser <garcia_mendez_diego@hotmail.com>
2021-10-20 00:05:13 -0300

    se agrega filtro a elementos filtrados

--- Pages/ProcesoFiltro.php
+++ Pages/ProcesoFiltro.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Style/estilo.css"/>
+    <link rel="stylesheet" href="../Style/estilo.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Birthstone&display=swap" rel="stylesheet">
@@ -18,6 +18,29 @@
     <!-- SECCION CONTENIDO -->
     <div class="contenedor">
         <?php include "../botones.inc" ?>
+        <div id="Filtro">
+            <fieldset id="FldConsultas">
+                <legend>Filtro</legend>
+                <form id="dataFRM" action="ProcesoFiltro.php" method="POST">
+                    <table id="TablaConsultas">
+                        <tr>
+                            <td>
+                                <h4>Origen:</h4>
+                            </td>
+                            <td>
+                                <input id="dataORG" style="text-transform:uppercase;" onkeyup="javascript:this.value=this.value.toUpperCase();" type="text" name="ORG" maxlenght="10" title="USA/CHINA/INDIA" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td id="botonesformulario" colspan="2">
+                                <input type="button" class="BotonesFormulario" value="Buscar" onclick="Filtro();" />
+                                <input type="reset" class="BotonesFormulario" value="Cancelar" />
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+            </fieldset>
+        </div>
         <div id="Filtrado">
             <form id="dataFRM" action="ProcesoFiltro.php" method="POST">
                 <table id="TablaConsultas">
@@ -81,10 +104,6 @@
                         }
                     }
                     ?>
-                    </fieldset>
-        </div>
-        <div>
-            <button type="button" class="BotonVolver" onclick="window.location.href='consultas.php'">Volver</button>
         </div>
 </body>
 
